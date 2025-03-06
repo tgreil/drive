b7cf21de87b6d028448de13746156ebe181c71ca
Nathan Vasse <nathan.vasse@gmail.com>
2025-03-06 16:38:06 +0100

    ✨(front) add actions on top of tree sidebar
    Those buttons will soon be plugged with the backend.

--- src/frontend/apps/drive/src/features/explorer/components/ExplorerTree.tsx
+++ src/frontend/apps/drive/src/features/explorer/components/ExplorerTree.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getDriver } from "@/features/config/config";
-
+import { Button } from "@openfun/cunningham-react";
+import { useTranslation } from "react-i18next";
 export const ExplorerTree = () => {
+  const { t } = useTranslation();
   const driver = getDriver();
   console.log("ExplorerTree", driver);
   const { data } = useQuery({
@@ -12,6 +14,19 @@ export const ExplorerTree = () => {
 
   return (
     <div>
+      <div className="explorer__tree__actions">
+        <div className="explorer__tree__actions__left">
+          <Button icon={<span className="material-icons">add</span>}>
+            {t("explorer.tree.createFolder")}
+          </Button>
+          <Button color="secondary">{t("explorer.tree.import")}</Button>
+        </div>
+        <Button
+          color="primary-text"
+          aria-label={t("explorer.tree.search")}
+          icon={<span className="material-icons">search</span>}
+        ></Button>
+      </div>
       <h4>Explorer Tree</h4>
       <div>
         {data?.map((item) => (
