392b9c6b624fb2879a3ae7ff98bbfe875f686ed0
Nathan Vasse <nathan.vasse@gmail.com>
2025-03-11 16:31:59 +0100

    ✨(front) plug Breadcrumbs to actual data
    Nothing great to say here, the title says it all at this point.

--- src/frontend/apps/drive/src/features/explorer/components/ExplorerBreadcrumbs.tsx
+++ src/frontend/apps/drive/src/features/explorer/components/ExplorerBreadcrumbs.tsx
@@ -1,51 +1,45 @@
 import { Breadcrumbs } from "@/features/ui/components/breadcrumbs/Breadcrumbs";
 import { Button } from "@openfun/cunningham-react";
-import Link from "next/link";
 import workspaceLogo from "@/assets/workspace_logo.svg";
+import { NavigationEventType, useExplorer } from "./ExplorerContext";
+import { getAncestors } from "../utils/tree";
+import { useMemo } from "react";
 
 export const ExplorerBreadcrumbs = () => {
+  const { tree, item, onNavigate } = useExplorer();
+
+  const getBreadcrumbsItems = () => {
+    if (!tree || !item) {
+      return [];
+    }
+
+    const ancestors = getAncestors(tree, item);
+    return ancestors.map((ancestor, index) => {
+      return {
+        content: (
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              onNavigate({
+                type: NavigationEventType.ITEM,
+                item: ancestor,
+              });
+            }}
+            className="c__breadcrumbs__button"
+          >
+            {index === 0 && <img src={workspaceLogo.src} alt="Lasuite" />}
+            {ancestor.title}
+          </button>
+        ),
+      };
+    });
+  };
+
+  const breadcrumbsItems = useMemo(() => getBreadcrumbsItems(), [tree, item]);
+
   return (
     <div className="explorer__content__breadcrumbs">
-      <Breadcrumbs
-        items={[
-          {
-            content: (
-              <Link href="/explorer" className="c__breadcrumbs__button">
-                <img src={workspaceLogo.src} alt="Lasuite" />
-                Mon Espace
-              </Link>
-            ),
-          },
-          {
-            content: (
-              <Link href="/explorer" className="c__breadcrumbs__button">
-                Documents
-              </Link>
-            ),
-          },
-          {
-            content: (
-              <Link href="/explorer" className="c__breadcrumbs__button">
-                Projet 2024
-              </Link>
-            ),
-          },
-          {
-            content: (
-              <Link href="/explorer" className="c__breadcrumbs__button">
-                Resources juridiques
-              </Link>
-            ),
-          },
-          {
-            content: (
-              <Link href="/explorer" className="c__breadcrumbs__button">
-                Réglementation
-              </Link>
-            ),
-          },
-        ]}
-      />
+      <Breadcrumbs items={breadcrumbsItems} />
       <div className="explorer__content__breadcrumbs__actions">
         <Button
           icon={<span className="material-icons">info</span>}
