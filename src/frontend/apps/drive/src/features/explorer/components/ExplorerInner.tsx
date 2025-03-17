50e196846a98b518805fd87378d449a119170538
Nathan Vasse <nathan.vasse@gmail.com>
2025-03-17 15:45:36 +0100

    ✨(front) add upload files
    We can now drag n drop files anywhere in order to trigger an upload
    to the current folder. This is a first version, we will improve it
    soon.

--- src/frontend/apps/drive/src/features/explorer/components/ExplorerInner.tsx
+++ src/frontend/apps/drive/src/features/explorer/components/ExplorerInner.tsx
@@ -3,13 +3,31 @@ import { ExplorerGrid } from "./ExplorerGrid";
 import { ExplorerBreadcrumbs } from "./ExplorerBreadcrumbs";
 import { useExplorer } from "./ExplorerContext";
 import { ExplorerSelectionBar } from "./ExplorerSelectionBar";
+import { useDropzone } from "react-dropzone";
+import clsx from "clsx";
+import { useMutation } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
+import { getDriver } from "@/features/config/Config";
+import { useEffect, useRef, useState } from "react";
+import {
+  addToast,
+  Toaster,
+  ToasterItem,
+} from "@/features/ui/components/toaster/Toaster";
+import { useTranslation } from "react-i18next";
+import { Id, toast } from "react-toastify";
+import { FileUploadToast } from "./toasts/FileUploadToast";
+export type FileUploadMeta = { file: File; progress: number };
 
 export const ExplorerInner = () => {
+  const { t } = useTranslation();
   const {
     setSelectedItemIds: setSelectedItems,
+    item,
     displayMode,
     selectedItems,
   } = useExplorer();
+  const driver = getDriver();
 
   const onSelectionStart = ({ event, selection }: SelectionEvent) => {
     if (!event?.ctrlKey && !event?.metaKey) {
@@ -37,6 +55,149 @@ export const ExplorerInner = () => {
     });
   };
 
+  const queryClient = useQueryClient();
+  const createFile = useMutation({
+    mutationFn: async (...payload: Parameters<typeof driver.createFile>) => {
+      await driver.createFile(...payload);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["items", item!.id],
+      });
+    },
+  });
+
+  const fileDragToastId = useRef<Id | null>(null);
+  const fileUploadsToastId = useRef<Id | null>(null);
+  const [uploadingState, setUploadingState] = useState<
+    Record<string, FileUploadMeta>
+  >({});
+
+  // Debug
+  // const [uploadingState, setUploadingState] = useState<
+  //   Record<string, FileUploadMeta>
+  // >({
+  //   "image.png": { file: new File([], "image.png"), progress: 0 },
+  //   "video.mp4": { file: new File([], "video.mp4"), progress: 0 },
+  //   "audio.mp3": { file: new File([], "audio.mp3"), progress: 0 },
+  //   "document.pdf": { file: new File([], "document.pdf"), progress: 0 },
+  //   "image2.png": { file: new File([], "image2.png"), progress: 0 },
+  //   "image3.png": { file: new File([], "image3.png"), progress: 0 },
+  //   "image4.png": { file: new File([], "image4.png"), progress: 0 },
+  //   "image5.png": { file: new File([], "image5.png"), progress: 0 },
+  // });
+  // useEffect(() => {
+  //   if (fileUploadsToastId.current) {
+  //     return;
+  //   }
+  //   fileUploadsToastId.current = addToast(
+  //     <FileUploadToast uploadingState={uploadingState} />,
+  //     {
+  //       autoClose: false,
+  //     }
+  //   );
+  //   const interval = setInterval(() => {
+  //     setUploadingState((prev) => {
+  //       return Object.fromEntries(
+  //         Object.entries(prev).map(([key, meta]) => [
+  //           key,
+  //           {
+  //             ...meta,
+  //             progress: meta.progress + Math.floor(Math.random() * 21) + 10,
+  //           },
+  //         ])
+  //       );
+  //     });
+  //   }, 1000);
+  // }, []);
+
+  useEffect(() => {
+    if (fileUploadsToastId.current) {
+      toast.update(fileUploadsToastId.current, {
+        render: <FileUploadToast uploadingState={uploadingState} />,
+      });
+    }
+  }, [uploadingState]);
+
+  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
+    useDropzone({
+      noClick: true,
+      // If we do not set this, the click on the "..." menu of each items does not work, also click + select on items
+      // does not work too. It might seems related to onFocus and onBlur events.
+      noKeyboard: true,
+      onDragEnter: () => {
+        if (fileDragToastId.current) {
+          return;
+        }
+        fileDragToastId.current = addToast(
+          <ToasterItem>
+            <span className="material-icons">cloud_upload</span>
+            <span>
+              {t("explorer.actions.upload.toast", { title: item?.title })}
+            </span>
+          </ToasterItem>,
+          { autoClose: false }
+        );
+      },
+      onDragLeave: () => {
+        if (fileDragToastId.current) {
+          toast.dismiss(fileDragToastId.current);
+          fileDragToastId.current = null;
+        }
+      },
+      onDrop: async (acceptedFiles) => {
+        if (fileDragToastId.current) {
+          toast.dismiss(fileDragToastId.current);
+          fileDragToastId.current = null;
+        }
+
+        if (!fileUploadsToastId.current) {
+          fileUploadsToastId.current = addToast(
+            <FileUploadToast uploadingState={uploadingState} />,
+            {
+              autoClose: false,
+              onClose: () => {
+                // We need to set this to null in order to re-show the toast when the user drops another file later.
+                fileUploadsToastId.current = null;
+              },
+            }
+          );
+        }
+
+        // Do not run "setUploadingState({});" because if a uploading is still in progress, it will be overwritten.
+
+        // First, add all the files to the uploading state in order to display them in the toast.
+        for (const file of acceptedFiles) {
+          setUploadingState((prev) => {
+            const newState = {
+              ...prev,
+              [file.name]: { file, progress: 0 },
+            };
+            return newState;
+          });
+        }
+
+        // Then, upload all the files sequentially. We are not uploading them in parallel because the backend
+        // does not support it, it causes concurrency issues.
+        for (const file of acceptedFiles) {
+          await createFile.mutateAsync({
+            filename: file.name,
+            file,
+            parentId: item!.id,
+            progressHandler: (progress) => {
+              setUploadingState((prev) => {
+                const newState = {
+                  ...prev,
+                  [file.name]: { file, progress },
+                };
+                return newState;
+              });
+            },
+          });
+        }
+      },
+    });
+
   return (
     <SelectionArea
       onStart={onSelectionStart}
@@ -54,7 +215,16 @@ export const ExplorerInner = () => {
         },
       }}
     >
-      <div className={`explorer explorer--${displayMode}`}>
+      <div
+        {...getRootProps({
+          className: clsx(`explorer explorer--${displayMode}`, {
+            "explorer--drop-zone--focused": isFocused,
+            "explorer--drop-zone--drag-accept": isDragAccept,
+            "explorer--drop-zone--drag-reject": isDragReject,
+          }),
+        })}
+      >
+        <input {...getInputProps()} />
         <div className="explorer__container">
           {selectedItems.length > 0 ? (
             <ExplorerSelectionBar />
@@ -67,6 +237,7 @@ export const ExplorerInner = () => {
             <ExplorerGrid />
           </div>
         </div>
+        <Toaster />
       </div>
     </SelectionArea>
   );
