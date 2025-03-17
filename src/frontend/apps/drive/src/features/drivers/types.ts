4321079d1f61105ab42b068665d1e415576ca9ce
Nathan Vasse <nathan.vasse@gmail.com>
2025-03-17 15:43:02 +0100

    🏷️(front) add types to Item
    We need to prepare the typings for upload policy.

--- src/frontend/apps/drive/src/features/drivers/types.ts
+++ src/frontend/apps/drive/src/features/drivers/types.ts
@@ -7,6 +7,17 @@ export type Item = {
   id: string;
   title: string;
   type: ItemType;
+  upload_state: string;
   updated_at: Date;
   children?: Item[];
+  policy?: {
+    url: string;
+    fields: {
+      AWSAccessKeyId: string;
+      acl: string;
+      policy: string;
+      key: string;
+      signature: string;
+    };
+  };
 };
