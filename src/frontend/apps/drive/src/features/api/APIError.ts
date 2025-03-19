ab8e77bc764342f5c01a19e1f98142fb48370f6a
Nathan Vasse <nathan.vasse@gmail.com>
2025-03-19 11:08:29 +0100

    ✨(front) display error toast
    When a react query query or mutation is trigerring an error we
    automatically display a toast with the most appropriate message
    possible. It is also possible to bypass this automatic behavior
    by giving a flag to the meta attribute of useQuery or useMutation.

--- src/frontend/apps/drive/src/features/api/APIError.ts
+++ src/frontend/apps/drive/src/features/api/APIError.ts
@@ -1,20 +1,43 @@
-interface IAPIError<T = unknown> {
-  status: number;
-  cause?: string[];
-  data?: T;
-}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import i18n from "@/features/i18n/initI18n";
 
-export class APIError<T = unknown> extends Error implements IAPIError<T> {
-  public status: IAPIError['status'];
-  public cause?: IAPIError['cause'];
-  public data?: IAPIError<T>['data'];
+export class APIError extends Error {
+  data?: any;
+  code: number;
 
-  constructor(message: string, { status, cause, data }: IAPIError<T>) {
-    super(message);
-
-    this.name = 'APIError';
-    this.status = status;
-    this.cause = cause;
+  constructor(code: number, data?: any) {
+    super();
     this.data = data;
+    this.code = code;
   }
 }
+
+export const errorToString = (error: unknown): string => {
+  if (typeof error === "string") {
+    return error;
+  }
+  if (error instanceof APIError) {
+    // If there is a data, it means that the error is a JSON object
+    if (error.data) {
+      return errorToString(error.data);
+    }
+    // If there is no data, it means that the error is a string, probably a complicated html error.
+    return i18n.t("api.error.unexpected");
+  }
+  if (error instanceof Error) {
+    return error.message;
+  }
+  if (typeof error === "object" && error !== null) {
+    /**
+     * This is made to handle full text errors from the API like:
+     *
+     * "title": [
+     *   "The title field is required."
+     *  ]
+     */
+    return Object.entries(error)
+      .map(([, value]) => `${errorToString(value)}`)
+      .join("\n");
+  }
+  return i18n.t("api.error.unexpected");
+};
--- src/frontend/apps/drive/src/pages/_app.tsx
+++ src/frontend/apps/drive/src/pages/_app.tsx
@@ -2,10 +2,21 @@ import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { CunninghamProvider } from "@gouvfr-lasuite/ui-kit";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  Query,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 import "../styles/globals.scss";
 import "../features/i18n/initI18n";
+import {
+  addToast,
+  ToasterItem,
+} from "@/features/ui/components/toaster/Toaster";
+import { errorToString } from "@/features/api/APIError";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,8 +25,30 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
+const onError = (error: Error, query: unknown) => {
+  if ((query as Query).meta?.noGlobalError) {
+    return;
+  }
+  addToast(
+    <ToasterItem type="error">
+      <span>{errorToString(error)}</span>
+    </ToasterItem>
+  );
+};
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: (error, variables, context, mutation) => onError(error, mutation),
+  }),
+  queryCache: new QueryCache({
+    onError: (error, query) => onError(error, query),
+  }),
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
