d18ab72723b571536b00e7285037d34e11e17857
Nathan Vasse <nathan.vasse@gmail.com>
2025-03-21 11:00:42 +0100

    ✨(front) add useMutations dedicated hook
    As we need to use this hook from multiple places, let's create
    a dedicated centralized mutation.

---
+++ src/frontend/apps/drive/src/features/explorer/hooks/useMutations.ts
@@ -0,0 +1,47 @@
+import { getDriver } from "@/features/config/Config";
+import { Item } from "@/features/drivers/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useExplorer } from "../components/ExplorerContext";
+export const useMutationDeleteItems = () => {
+  const driver = getDriver();
+  const queryClient = useQueryClient();
+  const { item } = useExplorer();
+  return useMutation({
+    mutationFn: async (...payload: Parameters<typeof driver.deleteItems>) => {
+      await driver.deleteItems(...payload);
+    },
+    onMutate: async (itemIds) => {
+      // Cancel any outgoing refetches
+      await queryClient.cancelQueries({
+        queryKey: ["items", item!.id, "children"],
+      });
+
+      // Snapshot the previous value
+      const previousItems = queryClient.getQueryData([
+        "items",
+        item!.id,
+        "children",
+      ]);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(["items", item!.id, "children"], (old: Item[]) =>
+        old ? old.filter((i: Item) => !itemIds.includes(i.id)) : old
+      );
+
+      // Return a context object with the snapshotted value
+      return { previousItems };
+    },
+    onError: (err, variables, context) => {
+      // If the mutation fails, use the context returned from onMutate to roll back
+      queryClient.setQueryData(
+        ["items", item!.id, "children"],
+        context?.previousItems
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["items", item!.id],
+      });
+    },
+  });
+};
