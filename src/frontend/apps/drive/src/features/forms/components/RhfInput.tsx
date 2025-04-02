d8b3bc89aa37c672cf8b1649aaadc7d73c2357f6
Nathan Vasse <nathan.vasse@gmail.com>
2025-04-02 15:34:49 +0200

    ✨(front) add RhfTextarea
    We need this wrapper for cunningham in order to provide rhf integration.

--- src/frontend/apps/drive/src/features/forms/components/RhfInput.tsx
+++ src/frontend/apps/drive/src/features/forms/components/RhfInput.tsx
@@ -1,6 +1,6 @@
 import { Controller, useFormContext } from "react-hook-form";
 import React from "react";
-import { Input } from "@openfun/cunningham-react";
+import { Input, TextArea, TextAreaProps } from "@openfun/cunningham-react";
 import { InputProps } from "@openfun/cunningham-react";
 
 export const RhfInput = (props: InputProps & { name: string }) => {
@@ -12,6 +12,29 @@ export const RhfInput = (props: InputProps & { name: string }) => {
       render={({ field, fieldState }) => {
         return (
           <Input
+            {...props}
+            aria-invalid={!!fieldState.error}
+            state={fieldState.error ? "error" : "default"}
+            text={fieldState.error?.message}
+            onBlur={field.onBlur}
+            onChange={(e) => setValue(field.name, e.target.value)}
+            value={field.value}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export const RhfTextarea = (props: TextAreaProps & { name: string }) => {
+  const { control, setValue } = useFormContext();
+  return (
+    <Controller
+      control={control}
+      name={props.name}
+      render={({ field, fieldState }) => {
+        return (
+          <TextArea
             {...props}
             aria-invalid={!!fieldState.error}
             state={fieldState.error ? "error" : "default"}
