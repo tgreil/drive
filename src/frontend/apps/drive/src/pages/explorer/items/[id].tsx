d7357b6d1529405917f550f7fbcee4ea845de8e4
Nathan Vasse <nathan.vasse@gmail.com>
2025-04-18 15:40:29 +0200

    ✨(front) handle filter on item route
    Implement the filter on the item route, simple.

--- src/frontend/apps/drive/src/pages/explorer/items/[id].tsx
+++ src/frontend/apps/drive/src/pages/explorer/items/[id].tsx
@@ -1,18 +1,31 @@
 import { getDriver } from "@/features/config/Config";
+import { ItemFilters } from "@/features/drivers/Driver";
 import { Explorer } from "@/features/explorer/components/Explorer";
 import { getGlobalExplorerLayout } from "@/features/layouts/components/explorer/ExplorerLayout";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function ItemPage() {
   const router = useRouter();
   const itemId = router.query.id as string;
+  const [filters, setFilters] = useState<ItemFilters>({});
   const { data: itemChildren } = useQuery({
-    queryKey: ["items", itemId, "children"],
-    queryFn: () => getDriver().getChildren(itemId),
+    queryKey: [
+      "items",
+      itemId,
+      "children",
+      ...(Object.keys(filters).length ? [JSON.stringify(filters)] : []),
+    ],
+    queryFn: () => getDriver().getChildren(itemId, filters),
   });
-
-  return <Explorer childrenItems={itemChildren} />;
+  return (
+    <Explorer
+      childrenItems={itemChildren}
+      filters={filters}
+      onFiltersChange={setFilters}
+    />
+  );
 }
 
 ItemPage.getLayout = getGlobalExplorerLayout;
